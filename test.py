94d31ed348514e69b60663a407ae3e1d0b957af6
PoojaDoctor <34757067+PoojaDoctor@users.noreply.github.com>
2019-03-02 22:49:07 -0800

    Flask now works

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/')
-def my_form():
-    return render_template('my-form.html')
+@app.route('/', methods=['GET', 'POST'])
 
-@app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.get("text")
-    processed_text = text.upper()
-    return processed_text
+    if request.method=='GET':
+        return render_template('text.html')
+    else:
+        text = request.args.get("name", " ")
+        print(text)
+        return text
 
 if __name__ == "__main__":
 	app.run()
